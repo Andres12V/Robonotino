ccd1d7c86323bc4a0f586275f66ef2cb5d1f9092
Andres12V <feli_610@hotmail.com>
2021-03-24 13:57:21 -0500

    Adding ArUco Detection 24 March

--- src/move_robotino/src/controller.py
+++ src/move_robotino/src/controller.py
@@ -11,11 +11,13 @@ import cv2 as cv
 import cv2.aruco as aruco
 import numpy as np
 import sys, time, math
+import imutils
 
 
 class Controller:
     def __init__(self):
         rospy.init_node('control')
+        self.sub_image = rospy.Subscriber('/my_robotino_urdf/camera/image_raw', Image, self.callback)
         self.sub = rospy.Subscriber('/odom', Odometry, self.odom_callback)
         self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
         self.rate = rospy.Rate(5000)
@@ -23,16 +25,14 @@ class Controller:
         self.msg = Twist()
         msg1 = Odometry()
 
-
-
     def odom_callback(self, msg1):
         pos_x = round(msg1.pose.pose.position.x)
         pos_y = round(msg1.pose.pose.position.y)
         print('Pos x,y', pos_x, pos_y )
 
-        if aruco_num == 7:
+        if aruco_num == 12:
             xd = 3.0
-            yd = -5.0
+            yd = -3.0
             K_p = 0.3
             K_i = 0.0003
 
@@ -46,16 +46,61 @@ class Controller:
             ey = yd-pos_y
             int_ey = int_ey+ey
             self.msg.linear.x = -ey*K_p
-
-
             rospy.loginfo('Looking for ArUco tag #'+str(aruco_num))
+        self.pub.publish(self.msg)
+        self.rate.sleep()
+    def callback(self, data):
+        try:
+            cv_image = self.bridge.imgmsg_to_cv2(data, 'bgr8')
+            cv.imshow('Image', cv_image)
+            aruco_image = cv_image
+            aruco_image = imutils.resize(aruco_image, width=500)
+            id_to_find= aruco_num
+            marker_size=10 #-[cm]
+            #  Get the camera calibration path
+            calib_path='/home/andresvergara/robotino_ws/src/Robotino/src/move_robotino/src/Calibpath/'
+            camera_matrix= np.loadtxt(calib_path+'cameraMatrix.txt', delimiter=',')
+            camera_distortion= np.loadtxt(calib_path+'cameraDistortion.txt', delimiter=',')
 
-        else:
+            #  180 deg rotation matrix aroud the x axis
+            R_flip= np.zeros((3,3), dtype=np.float32)
+            R_flip[0,0]=1.0
+            R_flip[1,1]=-1.0
+            R_flip[2,2]=-1.0
+
+            # Definde the aruco dictionary
+            aruco_dict=aruco.getPredefinedDictionary(aruco.DICT_ARUCO_ORIGINAL)
+            parameters= aruco.DetectorParameters_create()
+            font= cv.FONT_HERSHEY_PLAIN
+            #Convert in gray scale
+            gray=cv.cvtColor(aruco_image, cv.COLOR_BGR2GRAY)
+
+            # FInd all the aruco markers in the image
+            corners, ids, rejected=aruco.detectMarkers(image=gray, dictionary=aruco_dict, parameters=parameters)
+            aruco_image = aruco.drawDetectedMarkers(aruco_image, corners)
+            ret = aruco.estimatePoseSingleMarkers(corners, marker_size, camera_matrix, camera_distortion)
+            try:
+                rvec, tvec=ret[0][0,0,:], ret[1][0,0,:]
+                aruco.drawAxis(aruco_image, camera_matrix, camera_distortion, rvec, tvec, 10)
+                str_position= 'Marker Postion x=%4.0f  y=%4.0f  z=%4.0f'%(tvec[0]/10, tvec[1]/10, tvec[2]/10)
+                cv.putText(aruco_image, str_position, (0,100), font, 1, (0, 255, 0), 2, cv.LINE_AA)
+            except:
+                print('No ArUco Detected')
+
+            # Display the frame
+            cv.imshow('frame', aruco_image)
+
+        except CvBridgeError as e:
+            print(e)
+        k = cv.waitKey(1)
+        if k == ord('r'):
             self.msg.linear.x = 0
             self.msg.linear.y = 0
             self.msg.angular.z = 0
-        self.pub.publish(self.msg)
+            rospy.signal_shutdown('shutdown')
+            cv.destroyAllWindows()
         self.rate.sleep()
+
 
 
 if __name__ == '__main__':
